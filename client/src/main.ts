070a9d54094cce015a6e899e31ef41b2f6d2ec5e
Rafael Chipitelli <you@example.comRafaelChipitelli.o.c27@gmail.com>
2026-04-25 10:43:25 +1000

    login issue

--- client/src/main.ts
+++ client/src/main.ts
@@ -5,15 +5,8 @@ import './style.css';
 
 applyThemeCssVariables();
 
-const game = new Phaser.Game(createGameConfig());
+new Phaser.Game(createGameConfig());
 
-const resize = () => {
-  game.scale.resize(window.innerWidth, window.innerHeight);
-  game.scale.refresh();
-};
-
-window.addEventListener('resize', resize);
-window.addEventListener('orientationchange', resize);
 
 
 
